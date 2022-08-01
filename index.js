7f711633ad3cafacbc7c8e4bb5ec73801f62b0cc
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-08-01 11:50:44 -0500

    Desktop menu merging

---
+++ index.js
@@ -0,0 +1,12 @@
+const $ = (selector) => document.querySelector(selector);
+
+const menuEmail = $('.navbar-email');
+const desktopMenu = $('.desktop-menu');
+
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+  desktopMenu.classList.toggle('inactive');
+
+}
