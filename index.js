b7a27dd960e5100b8e7a32bcaa1915c242f914c6
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-08-01 12:26:07 -0500

    Mobile menu merging

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const $ = (selector) => document.querySelector(selector);
 
 const menuEmail = $('.navbar-email');
 const desktopMenu = $('.desktop-menu');
+const burgerMenu = $('.menu');
+const mobileMenu = $('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+}
 
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
 }
