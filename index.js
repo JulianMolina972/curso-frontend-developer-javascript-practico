2a79f498fb7c6c4ed9429bca532f6532a037dbab
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-08-01 13:27:33 -0500

    shopping cart

--- index.js
+++ index.js
@@ -2,17 +2,50 @@ const $ = (selector) => document.querySelector(selector);
 
 const menuEmail = $('.navbar-email');
 const desktopMenu = $('.desktop-menu');
+const menuCarIcon = $('.navbar-shopping-cart');
+const aside = $('.product-detail');
 const burgerMenu = $('.menu');
 const mobileMenu = $('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+menuCarIcon.addEventListener('click', toggleCarAside);
 
 function toggleDesktopMenu() {
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed){
+    aside.classList.add('inactive');
+  }
+
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed){
+    aside.classList.add('inactive');
+  }
+
+
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarAside() {
+
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+  if (!isMobileMenuClosed ){
+    mobileMenu.classList.add('inactive');
+  }
+
+  if (!isDesktopMenuClosed) {
+    desktopMenu.classList.add('inactive');
+  }
+
+  aside.classList.toggle('inactive');
 }
